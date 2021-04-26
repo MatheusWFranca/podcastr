4b521b595b3d0387855b9ce1a899890d534fa8fd
Matheus França <mateusanches2@gmail.com>
2021-04-25 23:05:53 -0300

    Estilização Episódio

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps, GetStaticPaths } from 'next';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
@@ -26,11 +26,13 @@ type EpisodeProps = {
 
 export default function Episode({ episode }: EpisodeProps) {
   return (
-    <div className={styles.episodes}>
+    <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Voltar" />
-        </button>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
+        </Link>
         <Image
           width={700}
           height={160}
