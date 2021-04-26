8319a62e8eb9977090631bb31c880ea21b76a395
Matheus França <mateusanches2@gmail.com>
2021-04-25 22:42:40 -0300

    Slugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -133,7 +132,6 @@ export const getStaticProps: GetStaticProps = async () => {
       durationAsString: convertDurationToTimeString(
         Number(episode.file.duration),
       ),
-      description: episode.description,
       url: episode.file.url,
     };
   });
