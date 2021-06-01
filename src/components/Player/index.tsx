73fbbf399d670e7bba50edd0e291a076bac9372c
Matheus França <mateusanches2@gmail.com>
2021-05-31 23:28:39 -0300

    Slider Lib  / Estilos

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image';
+import Slider from 'rc-slider';
 import { useContext } from 'react';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
-
-
+import 'rc-slider/assets/index.css';
 
 export default function Player() {
-  const {episodeList, currentEpisodeIndex} = useContext(PlayerContext);
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -16,15 +17,36 @@ export default function Player() {
         <strong>Tocando agora {episode?.title}</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={192}
+            height={192}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
-      <footer className={styles.empty}>
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
